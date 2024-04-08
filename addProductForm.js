0db9a4b06274eeea33b8843b2d2182f76c883f05
Dragos Serban <dragoserban26@yahoo.com>
2024-04-08 19:33:04 +0300

    Am inceput sa creez formularul de trimitere dat

---
+++ addProductForm.js
@@ -0,0 +1,10 @@
+root.innerHTML = `
+
+<form>
+    <label for="name">Nume:</label>
+    <input type="text" name="name" id="name">
+    <label for="price">Pret:</label>
+    <input type="number" id="price" name="price">
+    <input type="submit" value="Adauga un telefon">
+</form>
+`; 
