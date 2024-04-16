61e9ef7db0bf71145f1c620405ac93c74b763adc
Dragos Serban <dragoserban26@yahoo.com>
2024-04-16 20:59:26 +0300

    Ne-am folosit de localStorage pentru afisare in pagina

--- addProductForm.js
+++ addProductForm.js
@@ -18,13 +18,18 @@ function addFormFunctionality () {
         eventDetails.preventDefault();
         const name = eventDetails.target.name.value;
         const price = eventDetails.target.price.value;
-        const phone = {
+        const newPhone = {
             name: name,
             price: price
         }
+        // PAS1: Extragem produsele din localStorage
+        const localStorageProducts = getProducts();
+        // PAS2: Modificam produsele
+        localStorageProducts.push(newPhone);
+        //PAS3: ADaugam produsele modificate in localStorage
+        setProducts(localStorageProducts);
 
-        products.push(phone);
-        renderProductList(products);
+        renderProductList(localStorageProducts);
 
     });
 };
