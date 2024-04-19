f1fbd18c92e9f2e4b4e6dbac9f5b5338e9ba3b00
Dragos Serban <dragoserban26@gmail.com>
2024-04-19 20:03:59 +0300

    Minutul 1:13

--- sortButtons.js
+++ sortButtons.js
@@ -7,23 +7,33 @@ window.addEventListener('load', addSortingFunctionality);
 function sortProducts(sortType) {
     switch(sortType) {
         case 'SORT_BY_PRICE':
+                function compare(phone1,phone2) {
+                    if (phone1.price < phone2.price) {
+                        return -1;
+                    }                    
+                    if (phone1.price > phone2.price) {
+                        return 1;
+                    }
+                    return 0;
+                }
             console.log('sortare dupa pret')
-          // code block
+            const localStorageProducts = getProducts();
+            localStorageProducts.sort(compare);
+            console.log(localStorageProducts);
+          
           break;
         case 'SORT_BY_NAME':
             console.log('sortare dupa nume')
-            // code block
+            
             break;
         default:
-          // code block
       }
 }
 
 function addSortingFunctionality() {
     const sortByPriceButton = document.querySelector('#sort-by-price');
     sortByPriceButton.addEventListener('click', function(){
-        
-        // setProducts('SORT_BY_PRICE');
+        setProducts('SORT_BY_PRICE');
     })
 
 }
